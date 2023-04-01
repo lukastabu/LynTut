76759aee5dbcb8fa03bb5e48e7fe22c11209b99a
Lukas <lukas.tabulevicius@gmail.com>
2023-04-01 09:13:32 +0300

    05. Doing Calculations

--- iphp/ch04/04_03/increment.php
+++ iphp/ch04/04_03/increment.php
@@ -4,3 +4,13 @@ $number++;
 echo $number . '<br>';
 ++$number;
 echo $number . '<br>';
+
+$result = $number++ * 2;
+echo 'result is ' . $result;
+echo '<br>';
+echo $number;
+echo '<br>';
+$result = ++$number * 2;
+echo 'result is ' . $result;
+echo '<br>';
+echo $number;
