73595ad483443fb81452baf1847b1e44900eed1a
Lukas <lukas.tabulevicius@gmail.com>
2023-04-01 10:39:19 +0300

    06. using loops

--- iphp/ch05/05_01/while.php
+++ iphp/ch05/05_01/while.php
@@ -5,5 +5,11 @@ $i = 0;
 while ($i < 10) {
     // increment counter
     $i++;
+    if ($i % 2 == !0) {
+        continue;
+    }
     echo $i . '<br>';
+    // if ($i == 6) {
+    //     break;
+    // }
 }
--- iphp/ch05/05_06/multiplication.php
+++ iphp/ch05/05_06/multiplication.php
@@ -8,7 +8,26 @@
 <body>
 <h1>Multiplication Table</h1>
 <table>
+<?php
+    echo '<tr>';
+    echo '<th></th>';
+    for ($i = 1; $i <= 12; $i++) :
+        echo "<th>$i</th>";
+    endfor;
+    echo '</tr>';
 
+    for ($row=1, $collumn = 1; $row <= 12 ; $row++) { 
+        echo '<tr>';
+        if ($collumn == 1) {
+            echo "<th>$row</th>";
+        } 
+        while ($collumn <= 12) {
+                echo '<td>' . $row*$collumn++ . '</td>';
+        }
+        echo '</tr>';
+        $collumn = 1;
+    }
+    ?>
 </table>
 </body>
 </html>
