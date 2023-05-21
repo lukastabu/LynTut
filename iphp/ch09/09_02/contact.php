a9bbd43105b3200967c9199198a1990f041ed98b
lukas.tabulevicius <lukas.tabulevicius@cortlex.com>
2023-05-21 12:55:38 +0300

    09_03 required validators working

--- iphp/ch09/09_02/contact.php
+++ iphp/ch09/09_02/contact.php
@@ -1,3 +1,12 @@
+<?php
+$errors = [];
+$missing = [];
+if (isset($_POST['send'])) {
+  $expected = ['name', 'email', 'comments'];
+  $required = ['name', 'comments'];
+  require './includes/mailProcess.php';
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,17 +17,32 @@
 
 <body>
 <h1>Contact Us</h1>
+<?php if ($errors || $missing) : ?>
+<p class="warning">PLEASE FIX THE INDICATED ITEM(S)</p>
+<?php endif ?>
 <form method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
   <p>
-    <label for="name">Name:</label>
+    <label for="name">Name:
+      <?php if ($missing && in_array('name', $missing)) : ?>
+        <span class="warning">Please enter your name</span>
+      <?php endif ?> 
+    </label>
     <input type="text" name="name" id="name">
   </p>
   <p>
-    <label for="email">Email:</label>
+    <label for="email">Email:
+      <?php if ($missing && in_array('email', $missing)) : ?>
+        <span class="warning">Please enter your email</span>
+      <?php endif ?> 
+    </label>
     <input type="email" name="email" id="email">
   </p>
   <p>
-    <label for="comments">Comments:</label>
+    <label for="comments">Comments:
+      <?php if ($missing && in_array('comments', $missing)) : ?>
+        <span class="warning">Please enter your comments</span>
+      <?php endif ?> 
+    </label>
     <textarea name="comments" id="comments"></textarea>
   </p>
   <p>
